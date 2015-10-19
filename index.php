62339f7e52ddf12d8445ef2c5ed9910be8007d61
Jenny Crimp <jenny.crimp@gmail.com>
2015-10-19 12:18:55 -0700

    Customizes News page loop with excerpt, author, and date of post
    Testing web hook again.

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <!-- Start Content -->
     <div id="content">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-        <h1><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h1>
-        <?php the_content(''); // get page or posting written content ?>
+        <h1><a href="<?php the_permalink(); // link to the posting ?>"><?php the_title(); // get the posting title ?></a></h1>
+        <small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?></small>
+        <?php the_excerpt(); // get post excerpt ?>
         
         <?php endwhile; endif; // end the loop ?>
         <small>index.php</small> 
--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,15 @@
     <!-- Begin Primary Aside -->
     <aside id="primary">
-        <h2><a href="<?= get_permalink($post->post_parent) ?>"><?= get_the_title($post->post_parent) // Get the gateway page title ?></a></h2>
+        <?php if (is_page()) : ?>
+        
         <nav id="nav-sub">
+            <h2><a href="<?= get_permalink($post->post_parent) ?>"><?= get_the_title($post->post_parent) // Get the gateway page title ?></a></h2>
             <ul><?php if ($post->post_parent) { // Check if the page has a parent...
     wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __(''))); // if so, list the sub-pages with no title
     } else { // Check if the page does not have a parent
     wp_list_pages(array('child_of' => $post->ID, 'title_li' => __(''))); // if so, list the sub-pages with no title
     } ?></ul>
         </nav>
+<?php endif; ?>
     </aside> 
     <!-- End Primary Aside -->
