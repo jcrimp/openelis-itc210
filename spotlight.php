05e5b2ce0d2ed87254997aaeee8a45cf90ad6adc
Jenny Crimp <jenny.crimp@gmail.com>
2015-11-30 17:01:24 -0800

    Displays Leaflet map from custom field display as spotlight on Locations page
    Add a WYSIWYG custom field called ‘leaflet_map_shortcode’ in order to
    display the leaflet map in the spotlight area.

--- spotlight.php
+++ spotlight.php
@@ -6,8 +6,17 @@
             $featuredImage = wp_get_attachment_image_src($imgID, 'full' );//get the url of the featured image (returns an array)
             $imgURL = $featuredImage[0]; //get the url of the image out of the array       
         ?>
+        
         <img src="<?=$imgURL?>" alt="Spotlight Image" />
         <?php endif; ?>
+        <?php 
+        $leaflet = get_field( 'leaflet_map_shortcode' );
+        if(!empty($leaflet)): ?>
+            <div class="leaflet-map-container">
+                <?=$leaflet?>
+            </div>
+        <?php endif;
+        ?>
     
     <div class="row">
         <div class="large-12 columns">
